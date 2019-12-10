65d8d6f38fa1c4bb9201e2478acbd66a3b4a0a4c
Philippe Loctaux <p@philippeloctaux.com>
2019-12-11 00:21:55 +0100

    displaying date as title

--- src/root/root.ts
+++ src/root/root.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import dateformat from "dateformat";
 
 export const get = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    return res.render("root");
+    return res.render("root", {
+        date: dateformat(Date.now(), "dddd, mmmm dS")
+    });
 };
