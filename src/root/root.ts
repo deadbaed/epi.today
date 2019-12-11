469f6e59436d2168b9f97b243a19104a12901f02
Philippe Loctaux <p@philippeloctaux.com>
2019-12-11 23:01:41 +0100

    calling function from root and not from app

--- src/root/root.ts
+++ src/root/root.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import dateformat from "dateformat";
+import env from "../env";
+import { EventType, getEvents } from "../intra/event";
 
 export const get = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    let EventList: Array<EventType> = getEvents(<string>env.AUTOLOGIN, "2020", "01", "15");
+
+    EventList.forEach((event: EventType) => {
+        console.log(`${event.semester} ${event.module} ${event.name} ${event.registered}`);
+    });
 
     return res.render("root", {
         date: dateformat(Date.now(), "dddd, mmmm dS")
