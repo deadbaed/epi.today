119acd651f45c5e54d8a82fce7068e775c2ed8c0
Philippe Loctaux <p@philippeloctaux.com>
2019-12-21 19:01:24 +0100

    teasing for date selector + user needs to be logged in to log out

--- src/auth/routes.ts
+++ src/auth/routes.ts
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.get("/office365", auth.Office365);
 router.get("/callback", auth.Callback);
-router.get("/logout", auth.Logout);
+router.get("/logout", auth.isUserAuthenticated, auth.Logout);
 
 export default router;
