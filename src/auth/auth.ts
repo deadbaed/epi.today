5a698a291e9337ab998676c051ff832b195e6f36
Philippe Loctaux <p@philippeloctaux.com>
2019-12-21 18:50:28 +0100

    added passport via npm, added function to check if user is authenticated

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,6 +1,21 @@
 import express from "express";
 
 /**
+ * Check if user is authenticated or not
+ */
+export const isUserAuthenticated = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if (req.user) {
+        /* if user is logged in go to next function */
+        next();
+    } else {
+        /* return 403 error with explanation */
+        return res.status(403).render("errors/403", {
+            technical_error: "You are not logged in"
+        });
+    }
+};
+
+/**
  * Login use with Office 365
  */
 export const Office365 = (req: express.Request, res: express.Response, next: express.NextFunction) => {
--- src/date/routes.ts
+++ src/date/routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { isUserAuthenticated } from "../auth/auth";
 
 import * as date from "./date";
 
