18bb1e853ccb5794ddc7f9945405fa1c9c061e6e
Philippe Loctaux <p@philippeloctaux.com>
2019-12-24 03:34:00 +0100

    still can't get it to work, but i'm learning

--- src/authentication.ts
+++ src/authentication.ts
@@ -10,5 +10,9 @@ passport.use(new AzureStrategy({
     tenant: env.TENANT
 }, (accessToken: any, refreshToken: any, params: any, profile: any, done: any) => {
     console.log("passport callback fired");
-    console.log(params.id_token);
+    console.log("accessToken " + accessToken);
+    console.log("refreshToken " + refreshToken);
+    var waadProfile = jsonwebtoken.decode(params.id_token);
+    console.log(waadProfile);
+    // done(null, waadProfile);
 }));
