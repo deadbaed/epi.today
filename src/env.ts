7d2379a9816c03e8eb0f099486d9fc7d034d2831
Philippe Loctaux <p@philippeloctaux.com>
2019-12-26 21:23:09 +0100

    cleaner env.ts

--- src/env.ts
+++ src/env.ts
@@ -2,21 +2,13 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const { MODE, HOST, PORT, AUTOLOGIN, CLIENT_ID, CLIENT_SECRET, TENANT, COOKIE_SESSION } = process.env;
-
-if (MODE === "undefined" || HOST === "undefined" || PORT === "undefined" || AUTOLOGIN === "undefined" ||
-    CLIENT_ID === "undefined" || CLIENT_SECRET === "undefined" || TENANT === "undefined" || COOKIE_SESSION === "undefined") {
-    console.log("make sure you have a valid .env file");
-    process.exit(1);
-}
-
 export default {
-    MODE: MODE,
-    HOST: HOST,
-    PORT: PORT,
-    AUTOLOGIN: AUTOLOGIN,
-    CLIENT_ID: CLIENT_ID,
-    CLIENT_SECRET: CLIENT_SECRET,
-    TENANT: TENANT,
-    COOKIE_SESSION: COOKIE_SESSION
+    MODE: (process.env.MODE != "prod") ? "dev" : "prod",
+    HOST: process.env.HOST,
+    PORT: process.env.PORT,
+    AUTOLOGIN: process.env.AUTOLOGIN,
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    TENANT: process.env.TENANT,
+    COOKIE_SESSION: process.env.COOKIE_SESSION
 };
---
+++ src/modules.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        MODE: string;
+        HOST: string;
+        PORT: string;
+        AUTOLOGIN: string;
+        CLIENT_ID: string;
+        CLIENT_SECRET: string;
+        TENANT: string;
+        COOKIE_SESSION: string;
+    }
+}
